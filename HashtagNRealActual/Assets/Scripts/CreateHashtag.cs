96936a0c5de47108aa4321f56fd235ec951f5c7b
LWprogramming <LWprogramming@users.noreply.github.com>
2020-01-18 16:55:29 -0500

    Create UI screens

---
+++ HashtagNRealActual/Assets/Scripts/CreateHashtag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateHashtag : MonoBehaviour {
+	public GameObject welcomeScreen;
+	public GameObject createHashtagScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//switches screen to add hashtag
+	public void AddHashtag() {
+		Debug.Log("Clicked button! Attempting to switch screen");
+		welcomeScreen.SetActive(false);
+		createHashtagScreen.SetActive(true);
+	}
+}

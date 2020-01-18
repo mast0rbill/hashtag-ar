28a83b9ba9706db8b125d0906b89d4010ecee2db
mast0rbill <yuxuanliu@live.ca>
2020-01-17 21:46:20 -0500

    Implement get/post "restful" api

---
+++ HashtagNRealActual/Assets/Scripts/APITestDriver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APITestDriver : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.T)) {
+			HashtagAPI.instance.GetObjects("dummyLoc", "testTag", OnObjectRetrieval);
+		}
+		if(Input.GetKeyDown(KeyCode.J)) {
+			HashtagAPI.instance.PostObject("dummyLoc", "testTag", "image", "dummySource", OnPostError);
+		}
+	}
+
+	public void OnObjectRetrieval(HashtagObject[] objs) {
+		foreach(var obj in objs) {
+			Debug.Log(obj.location);
+		}
+	}
+
+	public void OnPostError(string e) {
+		Debug.LogError(e);
+	}
+}
---
+++ HashtagNRealActual/Assets/Scripts/HashtagAPI.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public struct HashtagObject {
+	public string location;
+	public string hashtag;
+	public string objType;
+	public string source;
+}
+
+public class HashtagAPI : MonoBehaviour {
+
+	public static HashtagAPI instance;
+
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		}
+	}
+
+	public void GetObjects(string location, string hashtag, Action<HashtagObject[]> then) {
+		StartCoroutine(GetRoutine(location, hashtag, then));
+	}
+
+	[System.Serializable]
+	struct HashtagArrayWrapper {
+		public HashtagObject[] objs;
+	}
+
+	private IEnumerator GetRoutine(string location, string hashtag, Action<HashtagObject[]> then) {
+		UnityWebRequest req = UnityWebRequest.Get("https://hashtag-vr-api.azurewebsites.net/api/ObjectController");
+		req.SetRequestHeader("location", location);
+		req.SetRequestHeader("hashtag", hashtag);
+
+		yield return req.SendWebRequest();
+
+		if(req.isNetworkError) {
+			Debug.LogError(req.error);
+		} else {
+			string formattedResp = $"{{\"objs\":{req.downloadHandler.text}}}";
+			HashtagObject[] hashtagObjs = ((HashtagArrayWrapper)JsonUtility.FromJson<HashtagArrayWrapper>(formattedResp)).objs;
+			then(hashtagObjs);
+		}
+	}
+
+	public void PostObject(string location, string hashtag, string objType, string source, Action<string> onError) {
+		StartCoroutine(PostRoutine(location, hashtag, objType, source, onError));
+	}
+
+	private IEnumerator PostRoutine(string location, string hashtag, string objType, string source, Action<string> onError) {
+		UnityWebRequest req = UnityWebRequest.Post("https://hashtag-vr-api.azurewebsites.net/api/ObjectController", "");
+		req.SetRequestHeader("location", location);
+		req.SetRequestHeader("hashtag", hashtag);
+		req.SetRequestHeader("objType", objType);
+		req.SetRequestHeader("source", source);
+
+		yield return req.SendWebRequest();
+
+		if(req.isNetworkError) {
+			onError(req.error);
+		} else {
+			Debug.Log("Post!");
+		}
+	}
+}

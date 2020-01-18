07c220c9fff64d2d6e9bcc0080eea8c49a799334
mast0rbill <yuxuanliu@live.ca>
2020-01-17 21:45:14 -0500

    Rewrite API to use headers

--- Server/Functions/ObjectController/__init__.py
+++ Server/Functions/ObjectController/__init__.py
@@ -6,9 +6,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     uri = "mongodb://hashtagvr:9GD5704BiJfgAhuNrkvvEar4WSGlBGL74tPSAHQEZg3vn6LnPhUN7EBrZAyCvqDT8ygnGLMloo0mMcql3cqh3w==@hashtagvr.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
     client = pymongo.MongoClient(uri)
 
-    reqBody = req.get_json()
-    location = reqBody["location"]
-    hashtag = reqBody["hashtag"]
+    headers = req.headers
+    
+    location = headers["location"]
+    hashtag = headers["hashtag"]
 
     db = client["hashtagvrdb"]
     col = db["objects"]
@@ -17,8 +18,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         collectionStr = col.find({"hashtag":hashtag})
         return func.HttpResponse(f"{dumps(collectionStr)}\n")
     elif req.method == 'POST':
-        objType = reqBody["objType"]
-        source = reqBody["source"]
+        objType = headers["objType"]
+        source = headers["source"]
         insertionDict = {"hashtag":hashtag, "location":location, "objType":objType, "source":source}
         col.insert_one(insertionDict)
         return func.HttpResponse(f"Request made: {str(insertionDict)}\n")

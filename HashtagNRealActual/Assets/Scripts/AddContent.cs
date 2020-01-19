d7cd1813c7c47f3e4448405c8abd07d64bb94f04
LWprogramming <LWprogramming@users.noreply.github.com>
2020-01-18 19:27:01 -0500

    textbox update

---
+++ HashtagNRealActual/Assets/Scripts/AddContent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddContent : MonoBehaviour {
+	public GameObject additionalContent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onClick() {
+		additionalContent.SetActive(true);
+	}
+}
